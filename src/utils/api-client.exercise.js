7efb938a6bbf090d3feceb5fb37329d2d63c41a5
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-01-26 23:44:12 +0100

    auth(ec4): update api-client to handle post requests

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,11 +1,15 @@
 import * as auth from 'auth-provider'
-
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, {token, headers: customHeaders, ...customConfig}) {
+function client(
+  endpoint,
+  {token, data, headers: customHeaders, ...customConfig},
+) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
+      'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
     },
