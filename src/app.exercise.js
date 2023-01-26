3bd5265b47c495d3132ae0f70c7da2dbf7ecee09
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-01-26 21:25:12 +0100

    auth(ec2): utilize useAsync to handle fetch and setting state safely
    By using our custom useAsync hook we can let that handle the fetching
    and setting state so we can utilize that returned state to render relevant
    components based on the fetching status

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,9 +3,12 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import * as colors from 'styles/colors'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 import {client} from 'utils/api-client.exercise'
+import {useAsync} from 'utils/hooks'
+import {FullPageSpinner} from 'components/lib'
 
 async function fetchUser() {
   let user = null
@@ -19,13 +22,20 @@ async function fetchUser() {
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync()
 
   React.useEffect(() => {
-    fetchUser().then(u => {
-      setUser(u)
-    })
-  }, [])
+    run(fetchUser())
+  }, [run])
 
   const login = form => auth.login(form).then(u => setUser(u))
 
@@ -36,11 +46,29 @@ function App() {
     setUser(null)
   }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isIdle || isLoading) {
+    return <FullPageSpinner />
+  } else if (isError) {
+    ;<div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  } else if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
