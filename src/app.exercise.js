e8877f333d5531f0b23ce07761daaf8999e5a3c6
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-02-23 07:37:30 +0100

    context(ec3): add AppProviders function to colocate global providers
    - AppProviders is now in charge of rendering our providers which we
      could move from index and app (react query, auth and router providers)
    - Cleans up our App even further, although now rendering Router around
      both authenticated and unauthenticated
    - Also cleans up our ReactDOM render
    - Doing this can help with testing

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,20 +1,13 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
-import {BrowserRouter as Router} from 'react-router-dom'
 import {useAuth} from './context/auth-context'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
 function App() {
   const {user} = useAuth()
-  return user ? (
-    <Router>
-      <AuthenticatedApp />
-    </Router>
-  ) : (
-    <UnauthenticatedApp />
-  )
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
 }
 
 export {App}
