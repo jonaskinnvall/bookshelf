e07a90cd35e235aefdbd077e2d50bfe1f81169a3
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-11 08:27:57 +0100

    performance(ec3): add production monitoring
    - Add performance monitoring using React.Profiler
    - To expand this component a bit we create our own Profiler that
      consumes React.Profiler so that we can sen along some additional props
      and data (phases and metadata)
    - We put all that we get from React.Profiler + our metadata into a queue
      and every 5 seconds we send that queue to our backend if it contains
      anything
    - To enable profiling in production using CRA we need to build with the
      --profile flag

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,43 @@
-// this is for extra credit
+import * as React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+setInterval(sendReport, 5000)
+
+function sendReport() {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+
+  const queueToSend = [...queue]
+  queue = []
+
+  return client('profile', {data: queueToSend})
+}
+
+function Profiler({phases, metadata, ...props}) {
+  function reportProfile(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+  ) {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        metadata,
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+      })
+    }
+  }
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
