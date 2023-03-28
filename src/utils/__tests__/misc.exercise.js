5b09f1d323e32317200683c7b26226589dc7e165
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-28 21:00:44 +0200

    unit-testing(ex): add unit tests for formatDate and client utilities

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date('March 28, 2023'))).toBe('Mar 23')
+})
