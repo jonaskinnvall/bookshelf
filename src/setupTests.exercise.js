85945bc63019cee586c6ed5b21c7777307b8423b
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-04-03 08:40:29 +0200

    unit-testing: move server setup into setupTests file
    - This way our server will be setup for all our tests from one file

--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,1 +1,5 @@
-// this isn't used in the solution. Only in the extra credit
+import {server} from 'test/server'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
